e17d9fe37301d07b4b8306e6d503c0f36043dd08
EstebanGomez1 <79277954+EstebanGomez1@users.noreply.github.com>
2024-04-14 14:15:54 +0200

    correcciones objetos mutables

--- Main.scala
+++ Main.scala
@@ -1,36 +1,33 @@
-object Main{
-  import aux.metodos
+object Main {
+  import auxiliar.metodos
   import scala.io.StdIn
+
+  def obtenerDimensiones(): (Int, Int) = {
+    try {
+      // Pedir al usuario que introduzca el valor de la fila
+      print("Introduce el valor de la fila: ")
+      val fila = StdIn.readInt()
+
+      // Pedir al usuario que introduzca el valor de la columna
+      print("Introduce el valor de la columna: ")
+      val columna = StdIn.readInt()
+
+      // Return dimensiones
+      (fila, columna)
+    } catch {
+      case e: NumberFormatException => {
+        println("Error: Debes introducir un número válido.")
+        // Llamar a la función de nuevo para permitir al usuario volver a intentarlo
+        obtenerDimensiones()
+      }
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     println("Hello world!")
 
-    var fila = 0
-    var columna = 0
-    val metodos = new Metodos()
-
-    def obtenerDimensiones(): (Int, Int) = {
-      try {
-        // Pedir al usuario que introduzca el valor de la fila
-        print("Introduce el valor de la fila: ")
-        fila = StdIn.readInt()
-
-        // Pedir al usuario que introduzca el valor de la columna
-        print("Introduce el valor de la columna: ")
-        columna = StdIn.readInt()
-      } catch {
-        case e: NumberFormatException => {
-          println("Error: Debes introducir un número válido.")
-          // Llamar a la función de nuevo para permitir al usuario volver a intentarlo
-          return obtenerDimensiones()
-        }
-      }
-      val dimension = fila * columna
-      println(metodos.llenarLista(dimension))
-      // Devolver las dimensiones como una tupla
-      (fila, columna)
-    }
-
-    obtenerDimensiones()
+    // Obtener las dimensiones del usuario
+    val (fila, columna) = obtenerDimensiones()
     println(s"Fila introducida: $fila, Columna introducida: $columna")
   }
 }
--- metodos.scala
+++ metodos.scala
@@ -1,8 +1,14 @@
-package aux{
+package auxiliar {
   class metodos {
     def llenarLista(dimension: Int): List[String] = dimension match {
       case 0 => Nil
-      case _ => " "::llenarLista(dimension-1)
+      case _ => " " :: llenarLista(dimension - 1)
     }
-
+    def suma(a: Int, b: Int): Int = {
+      return a + b
+    }
+    def resta(a: Int, b: Int): Int = {
+      return a - b
+    }
+  }
 }
